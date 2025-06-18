d6ca5c695f68e505af2688ed4cfad396be19b1e9
Wren00 <loren.walsh@unosquare.com>
2025-06-18 10:40:04 +0100

    Committing updated code
    # Conflicts:
    #	.gitignore
    #	README.md

---
+++ src/main/java/org/example/createanorc/controllers/CharacterController.java
@@ -0,0 +1,48 @@
+package org.example.createanorc.controllers;
+
+import java.util.ArrayList;
+import lombok.Generated;
+import org.example.createanorc.service.CharacterService;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+public class CharacterController {
+    private final CharacterService characterService;
+
+    @GetMapping({"/users/{user_id}/characters"})
+    @ResponseBody
+    public ArrayList<Character> retrieveAllCharacters(@PathVariable("user_id") int userId) {
+        return this.characterService.characterGETALL(userId);
+    }
+
+    @GetMapping({"/users/{user_id}/characters/{id}"})
+    @ResponseBody
+    public Character characterByID(@PathVariable("user_id") int userId, @PathVariable("id") int id) {
+        return this.characterService.characterGETBYID(userId, id);
+    }
+
+    @PostMapping({"/users/{user_id}/characters"})
+    @ResponseBody
+    public Character characterPost(@RequestBody Character model, @PathVariable("user_id") int userId) {
+        return this.characterService.characterPOST(model);
+    }
+
+    @DeleteMapping({"/users/{user_id}/characters/{character_id}"})
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void characterDelete(@PathVariable(name = "user_id") int userId, @PathVariable(name = "character_id") int characterId) {
+        this.characterService.characterDELETE(userId, characterId);
+    }
+
+    @Generated
+    public CharacterController(final CharacterService characterService) {
+        this.characterService = characterService;
+    }
+}
---
+++ src/test/java/org/example/createanorc/CreateAnOrcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.createanorc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateAnOrcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
