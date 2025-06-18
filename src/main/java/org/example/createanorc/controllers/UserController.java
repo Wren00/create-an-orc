515b461b50f03765dffb327a8b3feefb3f0e0f2f
Wren00 <loren.walsh@unosquare.com>
2025-06-18 17:24:47 +0100

    User posting methods and password encryption tested and working

--- src/main/java/org/example/createanorc/controllers/UserController.java
+++ src/main/java/org/example/createanorc/controllers/UserController.java
@@ -9,13 +9,7 @@ import org.example.createanorc.models.User;
 import org.example.createanorc.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -34,7 +28,7 @@ public class UserController {
         return this.userService.userGETBYID(id);
     }
 
-    @PostMapping({"/users/{id}"})
+    @PatchMapping({"/users/{id}"})
     @ResponseBody
     public User userPATCH(@PathVariable("id") int id, @RequestBody JsonNode updates) throws JsonProcessingException {
         return this.userService.userPATCH(id, updates);
